9018a4127e8045a495690947c019d60e15482a39
Rodrigo Rutkoski Rodrigues <rutkoski@gmail.com>
2016-03-29 09:32:23 -0300

    Amplify config file.

---
+++ app/config/amplify.php
@@ -0,0 +1,14 @@
+<?php
+if (! defined('SY_APP_DIR')) {
+    die();
+}
+
+/**
+ * Amplify tables prefix. Default is "amp_". Boolean false is empty.
+ */
+// $config['amp:tables_prefix'] = 'amp_';
+
+/**
+ * Admin routes prefix. Default is "/admin". Boolean false is empty.  
+ */
+// $config['amp:prefix'] = '/admin';
